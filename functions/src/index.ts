1468a3a046ce700f3189226a8d7c0501f1f20336
Daniele Sangalli <sangallidaniele@gmail.com>
2020-05-03 22:30:15 +0100

    adding post test

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,14 +1,38 @@
-import * as functions from 'firebase-functions';
-import * as firebase from 'firebase-admin';
+import * as functions from 'firebase-functions'
+import * as firebase from 'firebase-admin'
 
-firebase.initializeApp();
+firebase.initializeApp()
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
 export const helloWorld = functions.https.onRequest((request, response) => {
-
-    firebase.firestore().collection('items').onSnapshot((data) => {
-        response.send(data.docs[0].id);
+  firebase
+    .firestore()
+    .collection('items')
+    .onSnapshot((data) => {
+      response.send(data.docs[0].id)
     })
-});
+})
+
+export type Client = {
+  id?: string
+  name: boolean
+  createdAt: string
+}
+
+export const postOneClient = functions.https.onRequest((request, response) => {
+  const [name] = request.body
+
+  const newClient: Client = {
+    name,
+    createdAt: new Date().toISOString(),
+  }
+
+  firebase
+    .firestore()
+    .collection('clients')
+    .add(newClient)
+    .then((data) => response.json({ ...newClient, id: data.id }))
+    .catch((err: Error) => response.json(err))
+})
