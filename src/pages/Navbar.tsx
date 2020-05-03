5cfa263637b48d5f7803b0a33ea55f2dd3d8dcd9
Marco Sangalli <marco.sangalli@b3i.tech>
2020-05-03 22:08:27 +0200

    fixing header

--- src/pages/Navbar.tsx
+++ src/pages/Navbar.tsx
@@ -1,41 +1,46 @@
-import React from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { DEFAULT_PROVIDER, FIREBASE } from '../shared/firebase.config';
-import { Button } from 'grommet';
-import { Link } from 'react-router-dom';
+import { Anchor, Box, Button, Header, Text } from 'grommet'
+import React from 'react'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { Link } from 'react-router-dom'
+import { DEFAULT_PROVIDER, FIREBASE } from '../shared/firebase.config'
 
 export const Navbar = () => {
-  const [user, initialising, error] = useAuthState(FIREBASE.auth());
+  const [user, initialising, error] = useAuthState(FIREBASE.auth())
   const login = () => {
-    FIREBASE.auth().signInWithPopup(DEFAULT_PROVIDER);
-  };
+    FIREBASE.auth().signInWithPopup(DEFAULT_PROVIDER)
+  }
   const logout = () => {
-    FIREBASE.auth().signOut();
-  };
+    FIREBASE.auth().signOut()
+  }
 
   if (initialising) {
     return (
       <div>
         <p>Initialising User...</p>
       </div>
-    );
+    )
   }
   if (error) {
     return (
       <div>
         <p>Error: {error}</p>
       </div>
-    );
+    )
   }
-  if (user) {
-    return (
-      <div>
-        <p>Current User: {user.email}</p>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Button onClick={logout} label="Log out"/>
-      </div>
-    );
-  }
-  return <Button onClick={login} label="Log In"/>;
-};
+  return (
+    <Header background="brand">
+      <Box direction="row" align="center" gap="small">
+        {user ? <Text>Current User: {user.email}</Text> : null}
+      </Box>
+      <Box direction="row"  gap="medium" align="center" pad="medium">
+        <Anchor><Link to="/">Home</Link></Anchor>
+        <Anchor><Link to="/about">About</Link></Anchor>
+        {user ? (
+          <Button onClick={logout} label="Log out" />
+        ) : (
+          <Button onClick={login} label="Log In" />
+        )}
+      </Box>
+    </Header>
+  )
+}
