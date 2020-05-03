330be07be18741ab4e4d287e99696a806ba10284
Marco Sangalli <marco.sangalli@b3i.tech>
2020-05-03 22:48:57 +0200

    creating a function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,14 @@
 import * as functions from 'firebase-functions';
+import * as firebase from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+firebase.initializeApp();
+
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+
+    firebase.firestore().collection('items').onSnapshot((data) => {
+        response.send(data.docs[0].id);
+    })
+});
