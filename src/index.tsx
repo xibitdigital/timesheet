ad1ab391fd2f47163b09bd0d4459c4d6c335e4d9
Daniele Sangalli <sangallidaniele@gmail.com>
2020-05-08 17:12:52 +0100

    adding css reset

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { CssBaseline } from '@material-ui/core'
+
 import App from './App'
 import './index.css'
 
 ReactDOM.render(
-      <App />,
+  <>
+    <CssBaseline />
+    <App />
+  </>,
   document.getElementById('root')
 )
