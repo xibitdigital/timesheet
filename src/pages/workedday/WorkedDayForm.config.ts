71703c2326a9154529abb49a1d37e532fd3e0580
Marco Sangalli <marco.sangalli@b3i.tech>
2020-05-13 22:02:22 +0200

    renaming folder

--- src/pages/workedday/WorkedDayForm.config.ts
+++ src/pages/workedday/WorkedDayForm.config.ts
@@ -1,7 +1,6 @@
-import { FieldType } from '../../components/form/FormTypes'
+import { FieldType, FormConfig } from '../../components/form/FormTypes'
 import { requiredValidator } from '../../components/form/Validators'
 import { WorkedDay } from '../../shared/collections'
-import { FormConfig } from '../../components/form/FormTypes'
 
 export const WorkedDayFormConfig: FormConfig<WorkedDay> = [
   {
