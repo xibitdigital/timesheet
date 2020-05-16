c3b3258c12f4bc4474ca7acfc1c04d7289118f36
Daniele Sangalli <sangallidaniele@gmail.com>
2020-05-16 22:48:20 +0100

    refactor

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,20 +1,26 @@
 ///https://date.nager.at/api/v2/PublicHolidays/2020/GB
 
 import * as functions from 'firebase-functions'
-import { isValidDate, isValidCountry, getDateOfNextMonth } from './date'
+import {
+  isValidDate,
+  isValidCountry,
+  getDateOfNextMonth,
+  getFirstDayOfTheMonth,
+} from './date'
 import { DatesReqQueryType } from './types'
 
 exports.getDates = functions.https.onRequest(async (req, res) => {
   const { query } = req
   const { startDate, countryCode }: DatesReqQueryType = query
-  const endDate = getDateOfNextMonth(startDate)
+  const firstDayOfMonth = getFirstDayOfTheMonth(startDate)
+  const endDate = getDateOfNextMonth(firstDayOfMonth)
 
   if (
-    isValidDate(startDate) &&
+    isValidDate(firstDayOfMonth) &&
     isValidDate(endDate) &&
     isValidCountry(countryCode)
   ) {
-    // TODO check if startDate is already in the db
+    // TODO check if firstDayOfMonth is already in the db
 
     // TODO if not generate dates with getDays and insert them in the db
 
