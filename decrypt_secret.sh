2b46983bd76733795c42fb5ca6db3ecf1a71f6d1
Marco Sangalli <marco.sangalli@b3i.tech>
2020-05-29 20:29:15 +0200

    testing scripts

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -3,4 +3,6 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$REACT_ENV" \
---output $HOME/.env .env.gpg
+--output .env .env.gpg
+
+cat .env
