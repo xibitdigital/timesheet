f49bcfa7900ef383c5d984792f38b4aa06609f5c
Marco Sangalli <marco.sangalli@b3i.tech>
2020-06-05 22:22:36 +0200

    removing old folder

--- src/components/form/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './FormHelpers'
-export * from './FormHook'
-export * from './FormStateChart'
-export * from './FormTypes'
-export * from './Validators'
-export * from './FieldFactory'
