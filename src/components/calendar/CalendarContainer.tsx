2c9f80c4d7ab3832a80c6ce70a606fddf0bc4799
Marco Sangalli <marco.sangalli@b3i.tech>
2020-06-05 22:27:29 +0200

    fixing case

--- src/components/calendar/CalendarContainer.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-export const CalendarContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(7, 1fr);
-  grid-template-rows: repeat(53, min-content);
-  /* gap: 1em; */
-`
--- src/components/calendar/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './CalendarContainer'
-export * from './CalendarDay'
