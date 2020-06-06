b8b84e73b8ebb828be5afb691232b931e624d934
Marco Sangalli <sangalli.marco@gmail.com>
2020-06-06 23:13:33 +0200

    add on crea trigger

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -15,36 +15,46 @@ admin.initializeApp()
 
 const db = admin.firestore()
 
+const updateWorkedDays = async (
+  timeSheetId: string,
+  timesheet: TimeSheet
+): Promise<void> => {
+  const { year, month, countryCode, clientId, owner } = timesheet
+  const firstDayOfMonth = getFirstDayOfTheMonth(year, month)
+  const endDate = getLastDayOfTheMonth(firstDayOfMonth)
+
+  if (
+    isValidDate(firstDayOfMonth) &&
+    isValidDate(endDate) &&
+    isValidCountry(countryCode)
+  ) {
+    const days = await getDays(firstDayOfMonth, endDate, countryCode)
+    console.log(days)
+    const workDays = createWorkedDaysRecords(clientId, timeSheetId, owner)(days)
+
+    await deleteWorkedDays(db, timeSheetId)
+    await insertWorkDays(db, workDays)
+  }
+}
+
+exports.createWorkedDaays = functions.firestore
+  .document('timesheets/{timeSheetId}')
+  .onCreate(async (snapshoot, context) => {
+    console.log('Create')
+
+    const { timeSheetId } = context.params
+    const timesheet = snapshoot.data()
+
+    await updateWorkedDays(timeSheetId, timesheet as TimeSheet)
+  })
+
 exports.updateWorkedDays = functions.firestore
   .document('timesheets/{timeSheetId}')
   .onUpdate(async (change, context) => {
-    console.log('START')
-    console.log(context)
-    console.log(change.after.data())
+    console.log('Update')
 
     const { timeSheetId } = context.params
     const timesheet = change.after.data()
-    const { year, month, countryCode, clientId, owner } = timesheet as TimeSheet // add timesheet type here
 
-    const firstDayOfMonth = getFirstDayOfTheMonth(year, month)
-    const endDate = getLastDayOfTheMonth(firstDayOfMonth)
-
-    console.log('RANGE', firstDayOfMonth.toISOString(), endDate.toISOString())
-    console.log(firstDayOfMonth, endDate)
-
-    if (
-      isValidDate(firstDayOfMonth) &&
-      isValidDate(endDate) &&
-      isValidCountry(countryCode)
-    ) {
-      const days = await getDays(firstDayOfMonth, endDate, countryCode)
-      console.log(days)
-      const workDays = createWorkedDaysRecords(
-        clientId,
-        timeSheetId,
-        owner
-      )(days)
-      await deleteWorkedDays(db, timeSheetId)
-      await insertWorkDays(db, workDays)
-    }
+    await updateWorkedDays(timeSheetId, timesheet as TimeSheet)
   })
